9da33e4bb5f1c1ec8c331f40743d0bde1561e3e5
wjxjmj <wjxjmj@126.com>
2026-05-09 17:58:33 +0800

    fix: external links in Tauri via shell:open plugin

--- applications/web/src/components/AppBar.svelte
+++ applications/web/src/components/AppBar.svelte
@@ -9,6 +9,15 @@
 
   const log = (function () { const context = "[AppBar.svelte]"; const color="gray"; return Function.prototype.bind.call(console.log, console, `%c${context}`, `font-weight:bold;color:${color};`)})() // prettier-ignore
 
+  function openExternal(url: string) {
+    const tauriInternals = (window as any).__TAURI_INTERNALS__
+    if (tauriInternals) {
+      tauriInternals.invoke("plugin:shell|open", { path: url })
+    } else {
+      window.open(url, "_blank")
+    }
+  }
+
   let {
     project,
     renaming = false,
@@ -105,7 +114,7 @@
     <div
       class="hover:bg-gray-300 dark:hover:bg-gray-600 rounded p-1"
       onclick={() => {
-        window.open("https://github.com/wjxjmj/Trition3D/issues", "_blank")
+        openExternal("https://github.com/wjxjmj/Trition3D/issues")
       }}
     >
       <span class="h-6 w-6 block pointer-events-none">{@html Bug}</span>
@@ -140,10 +149,11 @@
       <div class="opacity-30 cursor-not-allowed" title="Coming soon">
         <span class="h-6 w-6 block pointer-events-none">{@html MessageCircle}</span>
       </div>
-      <div>
-        <a href="https://github.com/wjxjmj/Trition3D" target="_blank">
-          <svg class="h-6 w-6" viewBox="0 0 16 16" fill="currentColor"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/></svg>
-        </a>
+      <div
+        class="hover:bg-gray-300 dark:hover:bg-gray-600 rounded p-1 cursor-pointer"
+        onclick={() => openExternal("https://github.com/wjxjmj/Trition3D")}
+      >
+        <svg class="h-6 w-6 pointer-events-none" viewBox="0 0 16 16" fill="currentColor"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/></svg>
       </div>
     </div>
   </div>
