ee2e20268b8abc8b21eff29306e02c00656b994c
wjxjmj <wjxjmj@126.com>
2026-05-09 17:50:01 +0800

    fix: add pointer-events-none to icons, replace GitHub with octocat SVG

--- applications/web/src/components/AppBar.svelte
+++ applications/web/src/components/AppBar.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import {store} from "shared/stores.svelte"
   import fileDownload from "js-file-download"
-  import {Download, Upload, Bug, Sun, Moon, MessageCircle, CodeXml} from "lucide-static"
+  import {Download, Upload, Bug, Sun, Moon, MessageCircle} from "lucide-static"
   import type {WithTarget} from "shared/types"
   import {isProject} from "shared/typeGuards"
   import {base} from "../base"
@@ -89,13 +89,13 @@
         fileDownload(JSON.stringify(json), `${project.name}.tri`)
       }}
     >
-      <span class="h-6 w-6 block">{@html Download}</span>
+      <span class="h-6 w-6 block pointer-events-none">{@html Download}</span>
     </div>
 
     <!-- svelte-ignore a11y_no_static_element_interactions -->
     <div class="hover:bg-gray-300 dark:hover:bg-gray-600 rounded p-1">
       <label for="file-inp">
-        <span class="h-6 w-6 block">{@html Upload}</span>
+        <span class="h-6 w-6 block pointer-events-none">{@html Upload}</span>
         <input id="file-inp" type="file" hidden onchange={fileInput} />
       </label>
     </div>
@@ -108,7 +108,7 @@
         window.open("https://github.com/wjxjmj/Trition3D/issues", "_blank")
       }}
     >
-      <span class="h-6 w-6 block">{@html Bug}</span>
+      <span class="h-6 w-6 block pointer-events-none">{@html Bug}</span>
     </div>
 
     <!-- svelte-ignore a11y_no_static_element_interactions -->
@@ -127,7 +127,7 @@
         }
       }}
     >
-      <span class="h-6 w-6 block">
+      <span class="h-6 w-6 block pointer-events-none">
         {#if isDarkMode}
           {@html Moon}
         {:else}
@@ -138,11 +138,11 @@
 
     <div class="flex-grow flex flex-row-reverse gap-4 mr-4">
       <div class="opacity-30 cursor-not-allowed" title="Coming soon">
-        <span class="h-6 w-6 block">{@html MessageCircle}</span>
+        <span class="h-6 w-6 block pointer-events-none">{@html MessageCircle}</span>
       </div>
       <div>
         <a href="https://github.com/wjxjmj/Trition3D" target="_blank">
-          <span class="h-6 w-6 block">{@html CodeXml}</span>
+          <svg class="h-6 w-6" viewBox="0 0 16 16" fill="currentColor"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/></svg>
         </a>
       </div>
     </div>
